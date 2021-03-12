fef71061bd905ca374007ad8776256b8f5ea47e8
Trang Hoang <trang.d.hoang@gmail.com>
2021-03-12 04:09:10 +0000

    working code

--- studentSolution.cpp
+++ studentSolution.cpp
@@ -5,8 +5,29 @@
 #include "studentSolution.hpp"
 
 bool isPalindromeStack(std::string word) {
-	bool returnValue = true;
-	/* Your Code Here*/
-	return returnValue;
+	int length = word.length();
+    int mid = length / 2;
+    std::stack <char> s;
+
+    for (int i = 0; i < length; i++) { 
+        if (i < mid) {          // Traverse halfway
+            // Put character in stack - case insensitive
+            s.push(tolower(word.at(i))); 
+            if ((length % 2 != 0) && ((i + 1) == mid)) {
+                // If odd length, skip middle character
+                i++;            
+            }
+        } else {
+            // For remaining characters, compare with top 
+            // character in stack - case insensitive
+            if (tolower(word.at(i)) == s.top()) {
+                s.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+	return true;
 }
 #endif /* STUDENT_SOLUTION_CPP */
